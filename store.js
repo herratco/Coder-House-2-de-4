141153858caca797f3afa5aeb9c124da71240c6b
herratco <110746683+herratco@users.noreply.github.com>
2023-05-12 16:42:40 -0500

    Add files via upload

--- store.js
+++ store.js
@@ -1,49 +1,84 @@
 
 let loc = location.href.split("/")
 loc = loc[loc.length-1];
+//total should be a global variable since it is accessible in all directories 
+let cart =localStorage.getItem("store");
+cart = cart ? JSON.parse(cart):[];
 
 if(loc==="cart.html"){
-let cart =localStorage.getItem("store");
-if(cart){
-    cart = JSON.parse(cart)
-};
-console.log(cart)
-let total=0
+//before everything is done call the function 
+summeryCreator(cart);
 
+let total=0
 //create elements
 
 cart && cart.map(prod=>{
- total+=prod.totalPrice + prod.iva;
+
+ //destructuring is her /** what is destructuring spread out the property of the object */ 
+ const { totalPrice, iva, size, directory, price, quantity, id} = prod;
+
+ total+= totalPrice +  iva;
  document.write(
     `<div class='cart'>
-    <img src='${prod.directory}' alt='not' class='item'/>
-    <p>${prod.size}</p>
-    <p>$${prod.price}</p><p>${prod.quantity}</p>
-    <p>${prod.iva}</p>
-    <button id='remove' onclick='remove(${prod.id})'>Remove</button>
+    <img src='${ directory}' alt='not' class='item'/>
+    <p>${ size}</p>
+    <p>$${ price}</p><p>${ quantity}</p>
+    <p>${ iva}</p>
+    <button id='remove' onclick='remove(${ id})'>Remove</button>
     </div>`)
 })
 document.getElementById("total").innerHTML=`Total: $${total}`
 const button = document.getElementById('btn');
+const alertBox = document.getElementById("alert");
+const message = document.getElementById("message");
+
 button.addEventListener("click", (e)=>{
   e.preventDefault();
-  localStorage.clear();
-  location.reload();
-  alert("yeah successfully purchased!")
+
+  alertBox.style.display = "flex";
+  if(!cart.length){
+   message.innerHTML = "you have to add items in the cart first"
+   message.style.color = "red"
+  }
   
 })
 
+
+//add event to the alert box
+/**get the alert button */
+const purchase = document.getElementById('btn-alert');
+purchase.addEventListener("click",(e)=>{
+   e.preventDefault();
+   localStorage.clear();
+   alertBox.style.display = "none";
+//   here is summery of the items to be sold 
+
+   location.reload();
+})
+
 function remove (id){
-    const unRemoved = cart.filter(prod=>prod.id!==id);
+    const unRemoved = cart.filter(prod=> id!==prod.id);
    localStorage.setItem("store",JSON.stringify(unRemoved));
    location.reload();
 }
 
+
 }
+
+else if(loc==="index.html" || loc ==="nosotros.html")
+ {
+    //only call the summery creator function
+    summeryCreator(cart);
+ }
+
+   
 // this could be executed when the location out of cart 
 else{
-    const cart = document.getElementById("form");
-    cart.addEventListener("submit", async function(e){
+    //call the function to create a summery
+    summeryCreator(cart);
+
+    const form = document.getElementById("form");
+    form.addEventListener("submit", async function(e){
         //first we have to create stor
         e.preventDefault();
         const selected = document.getElementById("select");
@@ -84,25 +119,44 @@ else{
        const  IVA = 0.18;
        let store = localStorage.getItem("store")?JSON.parse(localStorage.getItem("store")):[];
        let iva = currentPrice * IVA;
-
-       store.push({
-        id:(!store.length)?1:store[store.length-1].id+1,
-        price,
-        directory: `img/${getItem()}`,
-        quantity:totalItem,
-        size:selected.value,
-        iva:parseFloat(iva.toFixed(2)),
-        totalPrice:parseFloat(currentPrice.toFixed(2)),//making into two decimal point
-    })
+       /**
+        * 
+        * JSON to modify the dom in the cart 
+        */
+       let newJsonItem = {
+        "id":(!store.length)?1:store.at(-1)["id"]+1,
+        "item":getItemName(),
+        "price":price,
+        "directory": `img/${getItem()}`,
+        "quantity":totalItem,
+        "size":selected.value,
+        "iva":parseFloat(iva.toFixed(2)),
+        "totalPrice":parseFloat(currentPrice.toFixed(2)),//making into two decimal point
+    }
+       
+    //spread operator here
+     /**
+        spread mean 
+             1) coping the items to add another item from existing one
+             2) coping the items to change/update the value when we it comes to object
+    */ 
+     
+       store = [
+        ...store,
+          newJsonItem
+       ]
 
     localStorage.setItem("store",JSON.stringify(store));
-    console.log(localStorage.getItem("store"))
+
+    // make fields come back to be empty
         selected.value = "";
         quantity.value = "";
        
        }
-       
+      location.reload() 
     })
+     //to make the number of items in the cart icon
+    
 
     function getItem(){
         let item = document.getElementsByTagName("img");
@@ -110,6 +164,51 @@ else{
         return item;
     }
     
+    function getItemName(){
+        let itemName = document.getElementsByTagName("h1");
+        return itemName[0].innerHTML;
+    }
 }
 
+
+function summeryCreator(store){
+const summeryBox = document.createElement("div")
+summeryBox.className = "item-counter";
+
+const cart = document.createElement("div");
+const icon = document.createElement("i");
+icon.className = "fa-solid fa-cart-shopping";
+
+const span = document.createElement("span");
+span.className = "count";
+span.innerText = store.length;
+
+cart.appendChild(icon);
+cart.appendChild(span);
+
+const details = document.createElement("details");
+const summery = document.createElement("summery");
+const ol = document.createElement("ol")
+
+const p = document.createElement("p");
+let total = 0;
+store.map(prod=>{
     
+    const {totalPrice,size,iva, item} = prod;
+    //  calculate total
+    total += totalPrice + iva
+
+    const li = document.createElement("li");
+    li.innerText = `${item} : ${size} :${totalPrice+iva}`
+    ol.appendChild(li)
+})
+p.innerText = `total: $${total}`;
+ol.appendChild(p);
+
+details.appendChild(summery)
+details.appendChild(ol);
+
+summeryBox.appendChild(cart)
+summeryBox.appendChild(details);
+document.body.appendChild(summeryBox)
+}
